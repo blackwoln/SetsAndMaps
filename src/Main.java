a41716a431428b78b0297b6205250c87f7b2f794
vkray <vitalikkrainyuk@yandex.ru>
2023-07-12 09:44:55 +0300

    master.  Коллекции: сеты и мапы. Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        task1();
+        task1();// Задача 1
         task2();
         task3();
         task4();
     }
 
     public static void task1 (){
+        System.out.println("Задача 1");
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+
+        for (int num : nums) {
+            if (num % 2 != 0) {
+                System.out.print(num + " ");
+            }
+        }
 
     }
 
